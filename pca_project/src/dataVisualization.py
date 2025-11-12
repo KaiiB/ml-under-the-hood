4c2736f2d018d6ea28fed9bf063fcb7ea1e6fbd7
ri2658 <ideryoryo@gmail.com>
2025-11-12 15:16:25 -0800

    Updated pca_project

--- pca_project/src/dataVisualization.py
+++ pca_project/src/dataVisualization.py
@@ -1,21 +1,37 @@
 import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
 from matplotlib.patches import Ellipse
+import numpy as np
 
-def createPlot(xlim=None, ylim=None, title='Plot'):
+def createPlot(xlim=None, ylim=None, title='Plot', show=True):
+    
+    if show == False:
+        # Prevent from showing
+        plt.ioff()
+
     # Create figure and axis
-    fig, ax = plt.subplots()
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    
+    # Set xlim and #ylim
     if xlim is not None:
-        ax.set_xlim(xlim[0], xlim[1])
+        ax.set_xlim(*xlim);
     if ylim is not None:
-        ax.set_ylim(ylim[0], ylim[1])
-    ax.set_aspect('equal')
-    ax.spines['left'].set_position('zero')
-    ax.spines['bottom'].set_position('zero')
-    ax.spines['right'].set_color('none')
-    ax.spines['top'].set_color('none')
-    plt.title(title)
-    plt.xlabel('X')
-    plt.ylabel('Y')
+        ax.set_ylim(*ylim);
+    
+    ax.set_aspect('equal');
+    ax.spines['left'].set_position('zero');
+    ax.spines['bottom'].set_position('zero');
+    ax.spines['right'].set_color('none');
+    ax.spines['top'].set_color('none');
+    plt.title(title);
+    plt.xlabel('X');
+    plt.ylabel('Y');
+
+    if show == False:
+        # Prevent from showing
+        plt.ion()
     return fig, ax
 
 def plotPoints(ax, x, y, **kwargs):
@@ -36,4 +52,120 @@ def runPlotActions(ax, actions, legend=True):
         func(ax, *args, **kwargs)
     if legend:
         ax.legend()
-        
+
+def createPlotlyPlot(data=None, x=None, y=None, xlim=None, ylim=None, title='Plot'):
+    if data is None or x is None or y is None:
+        # Create an empty figure if no data is given
+        fig = go.Figure()
+    else:
+        # Use px.scatter as the base figure
+        fig = px.scatter(
+            data_frame=data,
+            x=x,
+            y=y,
+            title=title,
+            labels={'x': 'X', 'y': 'Y'}
+        )
+    
+    # Format layout
+    fig.update_layout(
+        title=title,
+        xaxis=dict(
+            range=xlim,
+            zeroline=True,
+            zerolinewidth=2,
+            zerolinecolor='black'
+        ),
+        yaxis=dict(
+            range=ylim,
+            scaleanchor="x",  # equal aspect ratio
+            zeroline=True,
+            zerolinewidth=2,
+            zerolinecolor='black'
+        ),
+        template="simple_white",
+        width=600,
+        height=600,
+        showlegend=True
+    )
+
+    return fig
+
+def plotlyPoints(fig, x, y, name="Points", **kwargs):
+    # default marker
+    marker_defaults = dict(size=6)
+
+    # extract custom marker if passed
+    marker_custom = kwargs.pop("marker", {})
+    
+    # merge defaults and custom
+    marker_combined = {**marker_defaults, **marker_custom}
+
+    # if name is None, hide legend for this trace
+    showlegend = True if name is not None else False
+
+    fig.add_trace(go.Scatter(
+        x=x, y=y, mode='markers',
+        name=name,
+        marker=marker_combined,
+        showlegend=showlegend,
+        **kwargs
+    ))
+
+def plotlyQuiver(fig, x0, y0, u, v, name=None, **kwargs):
+    color = kwargs.get("color", "black")
+    width = kwargs.get("arrowwidth", 1.5)
+    size = kwargs.get("arrowsize", 1.5)
+    head = kwargs.get("arrowhead", 3)
+    # Draw arrows using annotations (these will stay visible)
+    for xi, yi, ui, vi in zip(x0, y0, u, v):
+        fig.add_annotation(
+            x=xi + ui, y=yi + vi,
+            ax=xi, ay=yi,
+            xref="x", yref="y",
+            axref="x", ayref="y",
+            showarrow=True,
+            arrowhead=head,
+            arrowsize=size,
+            arrowwidth=width,
+            arrowcolor=color
+        )
+    # Add a invisible trace for legend only if a name is specified
+    if name is not None:
+        fig.add_trace(go.Scatter(
+            x=[None], y=[None],
+            mode="lines",
+            line=dict(color=color, width=width),
+            name=name
+        ))
+
+def plotlyEllipse(fig, center, width, height, angle=0, name="Ellipse", **kwargs):
+    cx, cy = center
+    t = np.linspace(0, 2*np.pi, 200)
+    x = (width / 2) * np.cos(t)
+    y = (height / 2) * np.sin(t)
+    
+    # Apply rotation
+    R = np.array([
+        [np.cos(np.radians(angle)), -np.sin(np.radians(angle))],
+        [np.sin(np.radians(angle)),  np.cos(np.radians(angle))]
+    ])
+    xy = np.dot(R, np.vstack([x, y]))
+    x_rot, y_rot = xy[0] + cx, xy[1] + cy
+    
+    fig.add_trace(go.Scatter(
+        x=x_rot, y=y_rot,
+        mode='lines',
+        name=name,
+        line=dict(color=kwargs.get("color", "blue"))
+    ))
+
+def runPlotlyActions(fig, actions, legend=True):
+    for action in actions:
+        func = action["func"]
+        args = action.get("args", [])
+        kwargs = action.get("kwargs", {})
+        func(fig, *args, **kwargs)
+    if legend:
+        fig.update_layout(showlegend=True)
+    return fig
--- pca_project/src/pcaVisualize.py
+++ pca_project/src/pcaVisualize.py
@@ -1,63 +1,214 @@
 import numpy as np 
+import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.patches import Ellipse
+import plotly.graph_objects as go
 from ipywidgets import interact
-from src.dataBuilder import standardize, projection
-from src.dataVisualization import createPlot, plotPoints, plotQuiver, plotEllipse, runPlotActions
-from src.pcaCore import computePCA, pcaEllipse2D
+from src.dataBuilder import projection
+from src.dataVisualization import plotPoints, plotQuiver, plotEllipse, runPlotActions
+from src.dataVisualization import plotlyPoints, plotlyQuiver, plotlyEllipse, runPlotlyActions
 
-def sliderFunction(angle, data, eigvals, eigvecs, mean, width, height, length_pc, _z=2.0, seed=42, drawEllipse=False, drawPrincipalComponents=True):
-        W = eigvecs[:, :2] # Top 2 components
+def makeSliderFunction(fig, ax):
 
-        # Create the plot
-        fig, ax = createPlot(xlim=(-4, 4), ylim=(-4, 4), title='Can You Find the "Right" Principal Component?')
+    initial_xlim = ax.get_xlim()
+    initial_ylim = ax.get_ylim()
+    initial_title = ax.get_title()
+    initial_aspect = ax.get_aspect()
 
-        # Define all actions
+    def sliderFunction(
+            angle,
+            data,
+            eigvals,
+            eigvecs,
+            mean,
+            width,
+            height,
+            length_pc,
+            _z=2.0,
+            seed=42,
+            drawEllipse=False,
+            drawPrincipalComponents=True
+            ):
+            
+            # Top 2 components
+            W = eigvecs[:, :2]
+
+            # Clear old plot, reuse same figure
+            ax.clear()
+
+            # Reapply the axis settings from the original createPlot call
+            ax.set_xlim(initial_xlim)
+            ax.set_ylim(initial_ylim)
+            ax.set_aspect(initial_aspect if initial_aspect is not None else 'equal')
+
+            # reapply spines/labels/title exactly as createPlot does
+            ax.spines['left'].set_position('zero')
+            ax.spines['bottom'].set_position('zero')
+            ax.spines['right'].set_color('none')
+            ax.spines['top'].set_color('none')
+            ax.set_title(initial_title)
+            ax.set_xlabel('X')
+            ax.set_ylabel('Y')
+
+            # Define all actions
+            actions = [
+            {"func": plotPoints, "args": [data[:, 0], data[:, 1]], "kwargs": {"color": 'orange', "alpha": 0.6, "label": "Standardized Data"}},
+            ]
+
+            # Draw PCA ellipse if specified
+            if drawEllipse:
+                actions.append(
+                    {"func": plotEllipse, "args": [mean, width, height, angle], "kwargs": {"edgecolor": 'black', "facecolor": 'none', "linewidth": 2, "label": "Eigenvector 1"}} # projection vector
+                    )
+
+            # Draw principal components if specified
+            if drawPrincipalComponents:
+                # Note that we calculated eigenvectors for (x-mu)(x-mu)^T, not (x-mu)(x-mu)^T/N, so we need to divide by
+                # # N to get the true eigenvalues of the covariance matrix.
+                # Moreover, we wuliply by 2 because sqrt(eigenvalue) gives variance along that component,
+                # so 2*sqrt(eigenvalue/N) includes 2 std devs to cover ~95% of data along that component.
+
+                actions.append(
+                    {"func": plotQuiver, "args": [0,0,
+                                                    W[:, 0][0]*_z*np.sqrt(eigvals[0]/data.shape[0]),
+                                                    W[:, 0][1]*_z*np.sqrt(eigvals[0]/data.shape[0])],
+                                                    "kwargs": {"color": 'blue', "alpha": 0.6, "scale": 1, "label": "Eigenvector 1"}}
+                                                    ) # eigevnvector 1
+                actions.append(
+                    {"func": plotQuiver, "args": [0,0,
+                                                    -W[:, 0][0]*_z*np.sqrt(eigvals[0]/data.shape[0]),
+                                                    -W[:, 0][1]*_z*np.sqrt(eigvals[0]/data.shape[0])],
+                                                    "kwargs": {"color": 'blue', "alpha": 0.6, "scale": 1}}
+                                                    ) # eigevnvector 1
+                actions.append(
+                    {"func": plotQuiver, "args": [0,0,
+                                                    W[:, 1][0]*_z*np.sqrt(eigvals[1]/data.shape[0]),
+                                                    W[:, 1][1]*_z*np.sqrt(eigvals[1]/data.shape[0])],
+                                                    "kwargs": {"color": 'teal', "alpha": 0.6, "scale": 1, "label": "Eigenvector 2"}}
+                                                    ) # eigevnvector 2
+                actions.append(
+                    {"func": plotQuiver, "args": [0,0,
+                                                    -W[:, 1][0]*_z*np.sqrt(eigvals[1]/data.shape[0]),
+                                                    -W[:, 1][1]*_z*np.sqrt(eigvals[1]/data.shape[0])],
+                                                    "kwargs": {"color": 'teal', "alpha": 0.6, "scale": 1}}
+                                                    ) # eigevnvector 2
+
+            # For our slider, we want to paraterize direction in degrees.
+            direction = np.deg2rad(angle)
+            a = length_pc * np.cos(direction) 
+            b = length_pc * np.sin(direction)
+
+            # Because ax.quiver is a bit weird, we'll just draw two arrows in opposite directions for our principal components.
+            actions.append(
+                    {"func": plotQuiver, "args": [0, 0, a, b], 
+                    "kwargs": {"color": 'black', "scale": 1, "label": "User Direction"}}
+                    )
+            actions.append(
+                    {"func": plotQuiver, "args": [0, 0, -a, -b], 
+                    "kwargs": {"color": 'black', "scale": 1}}
+                    )
+            
+            # Draw residual vectors and projection points
+            np.random.seed(seed) # for reproducibility
+            for i in np.random.choice(np.arange(data.shape[0]), 20, replace=False):
+                    proj_i = projection(np.array([a, b]), data[i])
+                    # Residual vectors
+                    actions.append(
+                            {"func": plotQuiver, "args": [data[i][0], data[i][1], proj_i[0]-data[i][0], proj_i[1]-data[i][1],], "kwargs": {"scale": 1, "color": 'red'}}
+                    )
+                    # Projection points
+                    actions.append(
+                            {"func": plotPoints, "args": [proj_i[0], proj_i[1]], "kwargs": {"color": 'green', "alpha": 0.6}}
+                    )
+            # Run all actions
+            runPlotActions(ax, actions, True)
+
+            # Efficient redraw
+            fig.canvas.draw_idle()
+
+            return fig
+    return sliderFunction
+
+def makePlotlySliderFunction(fig):
+    # Store the initial layout settings to reapply each update
+    initial_layout = fig.layout.to_plotly_json()
+
+    def sliderFunctionPlotly(
+            angle,
+            data,
+            eigvals,
+            eigvecs,
+            mean,
+            width,
+            height,
+            length_pc,
+            _z=2.0,
+            seed=42,
+            drawEllipse=False,
+            drawPrincipalComponents=True
+        ):
+        # Copy top 2 eigenvectors
+        W = eigvecs[:, :2]
+
+        # Clear previous traces and annotations
+        fig.data = []
+        fig.layout.annotations = []
+
+        # Reapply layout settings (x/y limits, aspect ratio, etc.)
+        fig.update_layout(
+            xaxis=initial_layout["xaxis"],
+            yaxis=initial_layout["yaxis"],
+            title=initial_layout["title"],
+            template=initial_layout["template"],
+            width=initial_layout["width"],
+            height=initial_layout["height"],
+            showlegend=True
+        )
+
+        # Define all plot actions
         actions = [
-        {"func": plotPoints, "args": [data[:, 0], data[:, 1]], "kwargs": {"color": 'orange', "alpha": 0.6, "label": "Standardized Data"}},
+            {"func": plotlyPoints, "args": [data[:, 0], data[:, 1]], "kwargs": {"name": "Standardized Data", "marker": {"color": "orange", "opacity": 0.6}}},
         ]
 
         # Draw PCA ellipse if specified
         if drawEllipse:
             actions.append(
-                  {"func": plotEllipse, "args": [mean, width, height, angle], "kwargs": {"edgecolor": 'black', "facecolor": 'none', "linewidth": 2}} # projection vector
-                  )
+                {"func": plotlyEllipse, "args": [mean, width, height, angle], "kwargs": {"color": "black", "name": "Covariance Ellipse"}}
+            )
 
         # Draw principal components if specified
         if drawPrincipalComponents:
-            # Note that we calculated eigenvectors for (x-mu)(x-mu)^T, not (x-mu)(x-mu)^T/N, so we need to divide  by N to get the true eigenvalues of the covariance matrix
-            # Moreover, we wuliply by 2 because sqrt(eigenvalue) gives variance along that component, so 2*sqrt(eigenvalue/N) includes 2 std devs to cover ~95% of data along that component
-            actions.append(
-                  {"func": plotQuiver, "args": [0,0, W[:, 0][0]*_z*np.sqrt(eigvals[0]/data.shape[0]), W[:, 0][1]*_z*np.sqrt(eigvals[0]/data.shape[0])],"kwargs": {"color": 'blue', "alpha": 0.6, "scale": 1, "label": "Eigenvector 1"}}) # eigevnvector 1
-            actions.append(
-                  {"func": plotQuiver, "args": [0,0, -W[:, 0][0]*_z*np.sqrt(eigvals[0]/data.shape[0]), -W[:, 0][1]*_z*np.sqrt(eigvals[0]/data.shape[0])], "kwargs": {"color": 'blue', "alpha": 0.6, "scale": 1}}) # eigevnvector 1
-            actions.append(
-                  {"func": plotQuiver, "args": [0,0, W[:, 1][0]*_z*np.sqrt(eigvals[1]/data.shape[0]), W[:, 1][1]*_z*np.sqrt(eigvals[1]/data.shape[0])], "kwargs": {"color": 'teal', "alpha": 0.6, "scale": 1, "label": "Eigenvector 2"}}) # eigevnvector 2
-            actions.append(
-                  {"func": plotQuiver, "args": [0,0, -W[:, 1][0]*_z*np.sqrt(eigvals[1]/data.shape[0]), -W[:, 1][1]*_z*np.sqrt(eigvals[1]/data.shape[0])], "kwargs": {"color": 'teal', "alpha": 0.6, "scale": 1}}) # eigevnvector 2
+            # Scaling factor
+            scale_1 = _z * np.sqrt(eigvals[0] / data.shape[0])
+            scale_2 = _z * np.sqrt(eigvals[1] / data.shape[0])
 
-        # For our slider, we want to paraterize direction in degrees
+            # Eigenvector 1 (both directions)
+            actions.append({"func": plotlyQuiver, "args": [[0], [0], [W[0, 0] * scale_1], [W[1, 0] * scale_1]], "kwargs": {"color": "blue", "name": "Eigenvector 1"}})
+            actions.append({"func": plotlyQuiver, "args": [[0], [0], [-W[0, 0] * scale_1], [-W[1, 0] * scale_1]], "kwargs": {"color": "blue"}})
+
+            # Eigenvector 2 (both directions)
+            actions.append({"func": plotlyQuiver, "args": [[0], [0], [W[0, 1] * scale_2], [W[1, 1] * scale_2]], "kwargs": {"color": "teal", "name": "Eigenvector 2"}})
+            actions.append({"func": plotlyQuiver, "args": [[0], [0], [-W[0, 1] * scale_2], [-W[1, 1] * scale_2]], "kwargs": {"color": "teal"}})
+
+        # User-selected direction
         direction = np.deg2rad(angle)
-        a = length_pc * np.cos(direction) 
-        b = length_pc * np.sin(direction)
+        a, b = length_pc * np.cos(direction), length_pc * np.sin(direction)
+        actions.append({"func": plotlyQuiver, "args": [[0], [0], [a], [b]], "kwargs": {"color": "black", "name": "User Direction"}})
+        actions.append({"func": plotlyQuiver, "args": [[0], [0], [-a], [-b]], "kwargs": {"color": "black"}})
 
-        # Because ax.quiver is a bit weird, we'll just draw two arrows in opposite directions for our principal components
-        actions.append(
-                {"func": plotQuiver, "args": [0, 0, a, b], 
-                "kwargs": {"color": 'black', "scale": 1}})
-        actions.append(
-                {"func": plotQuiver, "args": [0, 0, -a, -b], 
-                "kwargs": {"color": 'black', "scale": 1}})
-        
-        np.random.seed(seed) # for reproducibility
-        for i in np.random.choice(np.arange(data.shape[0]), 20, replace=False):
-                proj_i = projection(np.array([a, b]), data[i])
-                actions.append(
-                        {"func": plotQuiver, "args": [data[i][0], data[i][1], proj_i[0]-data[i][0], proj_i[1]-data[i][1],], "kwargs": {"scale": 1, "color": 'red'}} # residual vector
-                )
-                actions.append(
-                        {"func": plotPoints, "args": [proj_i[0], proj_i[1]], "kwargs": {"color": 'green', "alpha": 0.6}} # projection points
-                )
-        # Run all actions
-        runPlotActions(ax, actions, True)
-        plt.show();
+        # Draw projection points and residuals
+        np.random.seed(seed)
+        sample_idx = np.random.choice(np.arange(data.shape[0]), 20, replace=False)
+        for i in sample_idx:
+            proj_i = projection(np.array([a, b]), data[i])
+            # Residual vectors
+            actions.append({"func": plotlyQuiver, "args": [[data[i][0]], [data[i][1]], [proj_i[0] - data[i][0]], [proj_i[1] - data[i][1]]], "kwargs": {"color": "red"}})
+            # Projection points
+            actions.append({"func": plotlyPoints, "args": [[proj_i[0]], [proj_i[1]]], "kwargs": {"name": None, "marker": {"color": "green", "opacity": 0.6}}})
+
+        # Execute all plotting actions
+        runPlotlyActions(fig, actions, legend=True)
+
+        return fig
+
+    return sliderFunctionPlotly
